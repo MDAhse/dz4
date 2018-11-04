7648275d42e49d45b66ac08dcfe93854207aa169
MDAhse <44751798+MDAhse@users.noreply.github.com>
2018-11-04 22:22:41 +0400

    Update text.py

--- text.py
+++ text.py
@@ -3,13 +3,13 @@ import re
 helpp = text.readline()
 result = re.sub(r'\?|\!','.', helpp)
 a = 1
-result = re.sub(r'\.','[\.\n]', result)
-for i in result:
-    result1 = re.sub(r'^', str(a), result)
-    a+=1
+result = re.sub(r'\.', '.\n', result)
 text1=open("text1.txt", "w")
-text1.write(result1)
+while re.search(r'^\D.+$', result) is not None:
+    result1 = re.search(r'^\D.+$', result)
+    result1 = re.sub(r'^', str(a), result1)
+    a+=1
+    text1.write(result1)
 text.close()
 text1.close()
-
 
