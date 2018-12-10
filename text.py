0cb07b5516d891501f8026919dfe2f60dd9b46b9
MDAhse <44751798+MDAhse@users.noreply.github.com>
2018-12-10 22:13:41 +0400

    Update text.py

--- text.py
+++ text.py
@@ -1,15 +1,19 @@
+from typing import Optional, Match
+
 text = open("text.txt","r")
 import re
 helpp = text.readline()
 result = re.sub(r'\?|\!','.', helpp)
 a = 1
-result = re.sub(r'\.', '.\n', result)
-text1=open("text1.txt", "w")
-while re.search(r'^\D.+$', result) is not None:
+
+result = re.split(r'[\n\.]', result)
+text1 = open("text1.txt", "w")
+for i in text1:
     result1 = re.search(r'^\D.+$', result)
     result1 = re.sub(r'^', str(a), result1)
     a+=1
     text1.write(result1)
 text.close()
 text1.close()
+
 
