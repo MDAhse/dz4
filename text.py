97ad241f98ca0a2dabda229775b53f2b8b4b7e85
MDAhse <44751798+MDAhse@users.noreply.github.com>
2018-12-11 00:28:46 +0400

    Update text.py

--- text.py
+++ text.py
@@ -6,14 +6,12 @@ helpp = text.readline()
 result = re.sub(r'\?|\!','.', helpp)
 a = 1
 
-result = re.split(r'[\n\.]', result)
+result = re.split(r'\.', result)
 text1 = open("text1.txt", "w")
-for i in result:
-    result1 = re.search(r'^\D.+$', str(result))
-    result1 = re.sub(r'^', str(a), str(result1))
-    a+=1
-    text1.write(result1)
+
+for i in range(0,len(result)-1):
+    text1.write(str(i+1))
+    text1.write(result[i]+'\n')
+
 text.close()
 text1.close()
-
-
